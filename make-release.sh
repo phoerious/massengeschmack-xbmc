d7160d78ab1253bc2dc3c9cb24ebb41233911cc7
Janek Bevendorff <janek@refining-linux.org>
2014-03-10 15:39:55 +0100

    Generate changelog.txt automatically

--- make-release.sh
+++ make-release.sh
@@ -6,6 +6,10 @@
 VERSION_NUMBER=$1
 VERSION_DESC=$2
 
+_CLR_GREEN='\e[1;32m'
+_CLR_RED='\e[1;31m'
+_CLR_NONE='\e[0m'
+
 while [[ "" == $VERSION_NUMBER ]]; do
     echo -n "Please enter the version number: "
     read VERSION_NUMBER
@@ -25,40 +29,63 @@ echo -n "Updating manifest file to version '${VERSION_NUMBER}' and committing ch
 read response
 
 if [[ "y" != $response ]] && [[ "Y" != $response ]]; then
-    echo "Exiting without changes."
+    echo -e "${_CLR_RED}Exiting without changes.${_CLR_NONE}"
     exit
 fi
 
 # Update manifest
-echo "Stashing working directory..."
+echo -e "${_CLR_GREEN}Stashing working directory...${_CLR_NONE}"
 git stash
-echo "Updating manifest..."
+echo -e "${_CLR_GREEN}Updating manifest...${_CLR_NONE}"
 manifest=$(< addon.xml)
 echo "$manifest" | sed 's/ name="Massengeschmack" version="[^"]\+" / name="Massengeschmack" version="'"${VERSION_NUMBER}"'" /' > addon.xml
 
-# Commit and tag changes
-echo "Committing changes..."
-git add addon.xml
-git commit -m "Bumping version number to ${VERSION_NUMBER}"
+# Update changelog
+echo "${_CLR_GREEN}Updating changelog...${_CLR_NONE}"
+changelog=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline -E -i --grep=" +cl$" --format=" - %s") | grep -Ev " -cl"
+if [[ "" == $changelog ]]; then
+    echo "${_CLR_RED}No commits marked for changelog inclusion, trying to assemble changelog automatically...${_CLR_NONE}" >&2
+    changelog=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline -E -i --grep="^(Add|Fix|Update|Remove) " --format=" - %s") | grep -Ev " -cl"
+fi
 
-echo "Tagging release..."
+if [[ "" == $changelog ]]; then
+    echo -e "${_CLR_RED}No changelog could be generated, skipping...${_CLR_NONE}" >&2
+else
+    oldChangelog=$(<changelog.txt)
+    newEntries=$(echo "$changelog" | sed 's/ +cl$//')
+    changelog="${VERSION_NUMBER}
+${changelog}
+
+${oldChangelog}"
+    echo "$changelog" > changelog.txt
+fi
+
+# Commit and tag changes
+echo -e "${_CLR_GREEN}Committing changes...${_CLR_NONE}"
+git add addon.xml
+git commit -m "Bump version number to ${VERSION_NUMBER} -cl"
+
+git add changelog.txt
+git commit -m "Update changelog -cl"
+
+echo -e "${_CLR_GREEN}Tagging release...${_CLR_NONE}"
 git tag -a "v${VERSION_NUMBER}" -m "Version ${VERSION_NUMBER} (${VERSION_DESC})"
 
-echo "Re-applying and popping stash..."
+echo -e "${_CLR_GREEN}Re-applying and popping stash...${_CLR_NONE}"
 git stash pop
 
 # Export ZIP file
 filename="plugin.video.massengeschmack-${VERSION_NUMBER}.zip"
-echo "Exporting release to '${filename}'..."
+echo -e "${_CLR_GREEN}Exporting release to '${filename}'...${_CLR_NONE}"
 if [ -e "${filename}" ]; then
     echo -n "File '${filename}' already exists. Overwrite? [y/N] "
     read response
     if [[ "y" != $response ]] && [[ "Y" != $response ]]; then
-        echo "Exiting without changes."
+        echo -e "${_CLR_RED}Exiting without changes.${_CLR_NONE}"
         exit
     fi
 fi
 
 git archive --output="plugin.video.massengeschmack-${VERSION_NUMBER}.zip" --format=zip --prefix="plugin.video.massengeschmack/" "v${VERSION_NUMBER}"
 
-echo "Done."
+echo -e "${_CLR_GREEN}Done.${_CLR_NONE}"
