d6d4135165de077acd566acee4707639b3ef069c
Janek Bevendorff <janek@refining-linux.org>
2016-02-19 21:29:08 +0100

    Update dates in license headers

--- resources/lib/datasource.py
+++ resources/lib/datasource.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # 
 # Massengeschmack XBMC add-on
-# Copyright (C) 2013 by Janek Bevendorff
+# Copyright (C) 2013-2016 by Janek Bevendorff
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -1077,7 +1077,7 @@ class AsynchronDataSource(DataSource):
                     False
                 )
             )
-        
+
         return listItems
     
     def getContentMode(self):
